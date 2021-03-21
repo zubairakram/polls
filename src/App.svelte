7bee15809738625f7c3f48fe6d1b0bde7393bbda
Zubair Akram <zubair616@gmail.com>
2021-03-21 22:57:07 +0500

    indentation corrected

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,38 @@
 <script>
     import Header from "./components/Header.svelte";
-import Footer from "./components/Footer.svelte";
-import PollList from "./components/PollList.svelte";
-import CreatPollForm from "./components/CreatPollForm.svelte";
-import Tabs from "./shared/Tabs.svelte";
+    import Footer from "./components/Footer.svelte";
+    import PollList from "./components/PollList.svelte";
+    import CreatPollForm from "./components/CreatPollForm.svelte";
+    import Tabs from "./shared/Tabs.svelte";
 
-let items = ['Current Polls', 'Add New Poll'];
-let activeItem = 'Current Polls';
+    let items = ['Current Polls', 'Add New Poll'];
+    let activeItem = 'Current Polls';
 
-function tabChange(e) {
-    activeItem = e.detail;
-}
+    function tabChange(e) {
+        activeItem = e.detail;
+    }
 
-
-function handleAdd () {
-    activeItem = 'Current Polls';
-}
+    function handleAdd () {
+        activeItem = 'Current Polls';
+    }
 
 </script>
 
-
 <Header></Header>
-    <main>
-        <Tabs {activeItem} {items} on:tabChange={ tabChange }></Tabs>
-            {#if activeItem === 'Current Polls'}
-                <PollList></PollList>
-            {:else if activeItem === 'Add New Poll'}
-                <CreatPollForm on:add={handleAdd}></CreatPollForm>
-            {/if}
-    </main>
+<main>
+    <Tabs {activeItem} {items} on:tabChange={ tabChange }></Tabs>
+    {#if activeItem === 'Current Polls'}
+        <PollList></PollList>
+    {:else if activeItem === 'Add New Poll'}
+        <CreatPollForm on:add={handleAdd}></CreatPollForm>
+    {/if}
+</main>
 <Footer></Footer>
 
 
 <style>
     main {
-        max-width: 960px;
-        margin: 40px auto;
+    max-width: 960px;
+    margin: 40px auto;
     }
 </style>
